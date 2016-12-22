e7fc61247dca1f3389cde2fbf7774e089b835b50
Chandu <basavapc@amazon.com>
2016-12-21 23:25:47 -0700

    Added cloudinary upload feature

--- BotWave.py
+++ BotWave.py
@@ -1,3 +1,7 @@
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 from flask import Flask
 import os
 
@@ -6,9 +10,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    cloudinary.config(
+        cloud_name="botsfloor",
+        api_key="521852823538172",
+        api_secret="Exvv_UaBxdvPIT7XmjOTFFAmyXM"
+    )
+    cloudinary.uploader.upload("images.jpeg")
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=False, port=port, host='0.0.0.0')
-    #app.run(debug=False, port=port, host='127.0.0.1')
+    #app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='127.0.0.1')
