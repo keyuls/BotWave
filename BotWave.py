03bfec759ba2475e2e28412843adb43f98fedbc9
Chandu <basavapc@amazon.com>
2017-01-08 11:57:13 -0700

    Updated database URL.

--- BotWave.py
+++ BotWave.py
@@ -14,7 +14,8 @@ cloudinary.config(
     )
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/postgres'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 toolsList =""
